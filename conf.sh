d6ecd324d92c1cd500d7e4c35b3fe2a3eb65c95a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-03-02 09:48:58 +0100

    Update registry name

--- conf.sh
+++ conf.sh
@@ -1,9 +1,9 @@
 # Build parameters
 # ----------------
-readonly REPO="gitlab-registry.in2p3.fr/astrolabsoftware/fink"
+readonly REPO="gitlab-registry.in2p3.fr/lsst/arrow-container-builder"
 # Tag to apply to the built image, or to identify the image to be pushed
 GIT_HASH="$(git -C $DIR describe --dirty --always)"
 readonly IMAGE_TAG="$GIT_HASH"
 # WARNING "spark-py" is hard-coded in spark build script
-readonly IMAGE="$REPO/parquet2csv:$IMAGE_TAG"
+readonly IMAGE="$REPO/arrow:$IMAGE_TAG"
 
